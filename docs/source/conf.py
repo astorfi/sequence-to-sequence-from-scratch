755691d297eb6df8e1c788b2f9763702ba6483fc
Amirsina Torfi <amirsina.torfi@gmail.com>
2019-01-02 20:05:51 -0500

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,6 +63,7 @@ locale_dirs = [
 ]
 gettext_compact = False
 
+html_show_sourcelink = False
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
